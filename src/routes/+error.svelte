87cc6539c45f39648480397a56c7bf5b0a39a6d7
William Wernert <william.wernert@gmail.com>
2023-03-14 19:09:18 -0400

    Show 404 for invalid page slug

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<div class="flex flex-col justify-center items-center w-full min-h-screen">
+  <h1 class="text-3xl">{$page.status}</h1>
+  <h2 class="text-2xl">{$page.error?.message}</h2>
+</div>
--- src/routes/orders/[page]/+page.server.ts
+++ src/routes/orders/[page]/+page.server.ts
@@ -1,7 +1,22 @@
 import prisma from "$lib/prisma";
+import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load = (async ({ params }) => {
+  if (isNaN(+params.page)) {
+    throw error(404, "Page does not exist");
+  }
+
+  const orderCount = await prisma.order.count();
+  const pageCount =
+    orderCount % 10 == 0
+      ? orderCount / 10
+      : Math.floor((orderCount + (10 - (orderCount % 10))) / 10);
+
+  if (Number(params.page) > pageCount) {
+    throw error(404, "Page does not exist");
+  }
+
   const orders = await prisma.order.findMany({
     skip: 10 * Number(params.page) - 10,
     take: 10,
@@ -9,8 +24,6 @@ export const load = (async ({ params }) => {
       id: "desc",
     },
   });
-
-  const orderCount = await prisma.order.count();
 
   return { orders, orderCount, page: Number(params.page) };
 }) satisfies PageServerLoad;
